cf58e6c0ce0e9930cb805851cdf64b5d090c536d
Eric Bram <61522167+sei-ebram@users.noreply.github.com>
2021-03-26 14:23:45 -0400

    cleaned up health check endpoints to reduce models in open api doc

--- src/Caster.Api/Features/HealthChecks/HealthCheckController.cs
+++ src/Caster.Api/Features/HealthChecks/HealthCheckController.cs
@@ -30,40 +30,32 @@ namespace Caster.Api.Features.HealthChecks
         /// Checks the liveliness health endpoint
         /// </summary>
         /// <remarks>
-        /// Returns a HealthReport of the liveliness health check
+        /// Returns a HealthStatus of the liveliness health check
         /// </remarks>
         /// <returns></returns>
         [HttpGet("live")]
-        [ProducesResponseType(typeof(HealthReport), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(HealthStatus), (int)HttpStatusCode.OK)]
         [SwaggerOperation(OperationId = "Health_GetLiveliness")]
         public async Task<IActionResult> GetLiveliness(CancellationToken ct)
         {
             HealthReport report = await this.healthCheckService.CheckHealthAsync((check) => check.Tags.Contains("live"));
-            var result = new
-            {
-                status = report.Status.ToString()
-            };
-            return report.Status == HealthStatus.Healthy ? this.Ok(result) : this.StatusCode((int)HttpStatusCode.ServiceUnavailable, result);
+            return report.Status == HealthStatus.Healthy ? this.Ok(report.Status) : this.StatusCode((int)HttpStatusCode.ServiceUnavailable, report.Status);
         }
 
         /// <summary>
         /// Checks the readiness health endpoint
         /// </summary>
         /// <remarks>
-        /// Returns a HealthReport of the readiness health check
+        /// Returns a HealthStatus of the readiness health check
         /// </remarks>
         /// <returns></returns>
         [HttpGet("ready")]
-        [ProducesResponseType(typeof(HealthReport), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(HealthStatus), (int)HttpStatusCode.OK)]
         [SwaggerOperation(OperationId = "Health_GetReadiness")]
         public async Task<IActionResult> GetReadiness(CancellationToken ct)
         {
             HealthReport report = await this.healthCheckService.CheckHealthAsync((check) => check.Tags.Contains("ready"));
-            var result = new
-            {
-                status = report.Status.ToString()
-            };
-            return report.Status == HealthStatus.Healthy ? this.Ok(result) : this.StatusCode((int)HttpStatusCode.ServiceUnavailable, result);
+            return report.Status == HealthStatus.Healthy ? this.Ok(report.Status) : this.StatusCode((int)HttpStatusCode.ServiceUnavailable, report.Status);
         }
     }
 } 
