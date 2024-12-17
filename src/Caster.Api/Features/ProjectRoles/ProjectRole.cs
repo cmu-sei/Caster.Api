52eb965a195b7dd2aec689bff3ee687fc215bd53
Andrew Schlackman <72105194+sei-aschlackman@users.noreply.github.com>
2024-12-17 16:16:21 -0500

    wip

--- src/Caster.Api/Features/ProjectRoles/ProjectRole.cs
+++ src/Caster.Api/Features/ProjectRoles/ProjectRole.cs
@@ -11,6 +11,7 @@ namespace Caster.Api.Features.ProjectRoles
         public Guid Id { get; set; }
 
         public string Name { get; set; }
+        public bool AllPermissions { get; set; }
 
         public Domain.Models.ProjectPermission[] Permissions { get; set; }
     }
