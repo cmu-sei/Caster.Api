31a40e0d61ee8ff0755556832409b485a53ca545
Andrew Schlackman <72105194+sei-aschlackman@users.noreply.github.com>
2024-12-20 14:07:07 -0500

    wip - default roles

--- src/Caster.Api/Domain/Models/SystemRole.cs
+++ src/Caster.Api/Domain/Models/SystemRole.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Microsoft.OpenApi.Extensions;
@@ -52,6 +53,8 @@ public enum SystemPermission
 public static class SystemRoleDefaults
 {
     public static Guid AdministratorRoleId = new("f35e8fff-f996-4cba-b303-3ba515ad8d2f");
+    public static Guid ContentDeveloperRoleId = new("d80b73c3-95d7-4468-8650-c62bbd082507");
+    public static Guid ObserverRoleId = new("1da3027e-725d-4753-9455-a836ed9bdb1e");
 }
 
 public class SystemRoleConfiguration : IEntityTypeConfiguration<SystemRole>
@@ -68,7 +71,29 @@ public class SystemRoleConfiguration : IEntityTypeConfiguration<SystemRole>
                 AllPermissions = true,
                 Immutable = true,
                 Permissions = [],
-                Description = "Can perform all actions"
+                Description = "Can perform all actions."
+            },
+            new SystemRole
+            {
+                Id = SystemRoleDefaults.ContentDeveloperRoleId,
+                Name = "Content Developer",
+                AllPermissions = false,
+                Immutable = false,
+                Permissions = [
+                    SystemPermission.CreateProjects
+                ],
+                Description = "Can create and manage their own Projects."
+            },
+            new SystemRole
+            {
+                Id = SystemRoleDefaults.ObserverRoleId,
+                Name = "Observer",
+                AllPermissions = false,
+                Immutable = false,
+                Permissions = Enum.GetValues<SystemPermission>()
+                    .Where(x => x.ToString().StartsWith("View"))
+                    .ToList(),
+                Description = "Can perform all View actions, but not make any changes."
             }
         );
     }
