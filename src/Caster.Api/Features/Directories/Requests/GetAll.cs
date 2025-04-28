878c360e9a7721cd53ae0d1adcef455e60c021a6
sei-tspencer <trspencer@sei.cmu.edu>
2025-04-28 11:28:50 -0400

    modify GetAll direectories to return my directories instead of 403 when ViewAllProjects is not granted

--- src/Caster.Api/Features/Directories/Requests/GetAll.cs
+++ src/Caster.Api/Features/Directories/Requests/GetAll.cs
@@ -1,10 +1,12 @@
 // Copyright 2021 Carnegie Mellon University. All Rights Reserved.
 // Released under a MIT (SEI)-style license. See LICENSE.md in the project root for license information.
 
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
 using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Caster.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using System.Runtime.Serialization;
@@ -36,18 +38,39 @@ namespace Caster.Api.Features.Directories
             public bool IncludeFileContent { get; set; }
         }
 
-        public class Handler(ICasterAuthorizationService authorizationService, IMapper mapper, CasterContext dbContext) : BaseHandler<Query, Directory[]>
+        public class Handler(
+            ICasterAuthorizationService authorizationService,
+            IMapper mapper,
+            CasterContext dbContext,
+            IIdentityResolver identityResolver) : BaseHandler<Query, Directory[]>
         {
-            public override async Task<bool> Authorize(Query request, CancellationToken cancellationToken) =>
-                await authorizationService.Authorize([SystemPermission.ViewProjects], cancellationToken);
+            public override Task<bool> Authorize(Query request, CancellationToken cancellationToken) => Task.FromResult(true);
 
             public override async Task<Directory[]> HandleRequest(Query request, CancellationToken cancellationToken)
             {
-                return await dbContext.Directories
-                    .Expand(mapper.ConfigurationProvider, request.IncludeRelated, request.IncludeFileContent)
-                    .ToArrayAsync();
+                if (await authorizationService.Authorize([SystemPermission.ViewProjects], cancellationToken))
+                {
+                    return await dbContext.Directories
+                        .Expand(mapper.ConfigurationProvider, request.IncludeRelated, request.IncludeFileContent)
+                        .ToArrayAsync();
+                }
+                else
+                {
+                    var userId = identityResolver.GetId();
+                    var myProjectIds = await dbContext.ProjectMemberships
+                        .Where(pm => pm.UserId == userId)
+                        .Select(pm => pm.ProjectId)
+                        .ToListAsync(cancellationToken);
+
+                    var myDirectories = await dbContext.Directories
+                        .Where(d => myProjectIds
+                        .Contains(d.ProjectId))
+                        .ProjectTo<Directory>(mapper.ConfigurationProvider)
+                        .ToArrayAsync(cancellationToken);
+
+                    return myDirectories;
+                }
             }
         }
     }
 }
-
