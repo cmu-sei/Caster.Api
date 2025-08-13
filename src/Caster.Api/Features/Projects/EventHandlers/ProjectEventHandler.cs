ed4313a93f95cef9f80964ade984d81d81e44581
sei-tspencer <trspencer@sei.cmu.edu>
2025-08-13 13:43:27 -0400

    real fix

--- src/Caster.Api/Features/Projects/EventHandlers/ProjectEventHandler.cs
+++ src/Caster.Api/Features/Projects/EventHandlers/ProjectEventHandler.cs
@@ -8,6 +8,7 @@ using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using Caster.Api.Data;
 using Caster.Api.Domain.Events;
+using Caster.Api.Domain.Services;
 using Caster.Api.Hubs;
 using MediatR;
 using Microsoft.AspNetCore.SignalR;
@@ -15,28 +16,29 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Caster.Api.Features.Projects.EventHandlers;
 
-public class ProjectCreatedSignalRHandler(CasterContext _db, IMapper _mapper, IHubContext<ProjectHub> _projectHub) :
-    ProjectBaseSignalRHandler(_db, _mapper, _projectHub),
+public class ProjectCreatedSignalRHandler(CasterContext _db, TelemetryService telemetryService) :
+    ProjectBaseSignalRHandler(_db, telemetryService),
     INotificationHandler<EntityCreated<Domain.Models.Project>>
 {
     public async Task Handle(EntityCreated<Domain.Models.Project> notification, CancellationToken cancellationToken)
     {
-        await base.Handle(notification.Entity, ProjectHubMethods.ProjectCreated, null, cancellationToken);
+        await base.Handle(notification.Entity, cancellationToken);
     }
 }
 
-public class ProjectDeletedSignalRHandler(IHubContext<ProjectHub> projectHub) :
+public class ProjectDeletedSignalRHandler(CasterContext _db, TelemetryService telemetryService) :
+    ProjectBaseSignalRHandler(_db, telemetryService),
     INotificationHandler<EntityDeleted<Domain.Models.Project>>
 {
     public async Task Handle(EntityDeleted<Domain.Models.Project> notification, CancellationToken cancellationToken)
     {
-        await projectHub.Clients.Group(ProjectHubMethods.GetProjectAdminGroup(notification.Entity.ProjectId)).SendAsync(ProjectHubMethods.ProjectDeleted, notification.Entity.Id);
+        await base.Handle(notification.Entity, cancellationToken);
     }
 }
 
-public class ProjectBaseSignalRHandler(CasterContext db, IMapper mapper, IHubContext<ProjectHub> projectHub)
+public class ProjectBaseSignalRHandler(CasterContext db, TelemetryService telemetryService)
 {
-    protected async Task Handle(Domain.Models.Project entity, string method, string[] modifiedProperties, CancellationToken cancellationToken)
+    protected async Task Handle(Domain.Models.Project entity, CancellationToken cancellationToken)
     {
         var count = await db.Projects.LongCountAsync(cancellationToken);
         telemetryService.Projects.Record((int)count);
