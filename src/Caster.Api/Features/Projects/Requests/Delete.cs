5afb700167b592dda18b5c4e18fa0b6e92e13594
Adam Welle <arwelle@sei.cmu.edu>
2026-06-02 20:00:19 +0000

    Add missing using directive for List<>

--- src/Caster.Api/Features/Projects/Requests/Delete.cs
+++ src/Caster.Api/Features/Projects/Requests/Delete.cs
@@ -2,6 +2,7 @@
 // Released under a MIT (SEI)-style license. See LICENSE.md in the project root for license information.
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
